f0e354ca6cd783ea8c28d229103bcd483776b5e0
Shakib448 <shakiba448@gmail.com>
2025-03-07 21:00:58 +0600

    Token and Lexer have added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::io::{self, Write};
+use std::iter::Peekable;
+use std::str::Chars;
+
+
+#[derive(Debug, PartialEq, Eq)]
+enum Token {
+    NUMBER(i32),
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    LEFT_PAREN,
+    RIGHT_PAREN,
 }
+
+struct Lexer<'a> {
+    chars : Peekable<Chars<'a>>
+}
+
+
+impl<'a> Lexer<'a> {
+    fn new(input : &'a str) -> Self {
+        Lexer {
+            chars : input.chars().peekable()
+        }
+    }
+
+}
+
+
+fn main() {}
